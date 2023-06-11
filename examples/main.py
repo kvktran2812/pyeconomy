76916b1f2e6fd6b4f0ab3aec0fb62e89725d4853
kvktran2812 <kvktran@gmail.com>
2023-06-11 13:31:11 -0400

    add examples

--- examples/main.py
+++ examples/main.py
@@ -1,13 +1,29 @@
-from src.market import Party
-from src.product import Product
+from typing import List
+
+from src.economy.product import Product
+from src.economy.market import Worker
+import matplotlib.pyplot as plt
+from src.economy.ppf import PPF
+import numpy as np
+from bokeh.plotting import figure, show
 
 
-if __name__ == "__main__":
-    party1 = Party(name="A")
-    party2 = Party(name="B")
+fish = Product("fish")
+apple = Product("apple")
 
-    party1.add_new_product(product=Product(name="fish", prize=1.5, lifetime="1mo"))
-    party1.add_new_product(product=Product(name="shrimp", prize=1.5, lifetime="1mo"))
-    party1.add_new_product(product=Product(name="crab", prize=1.5, lifetime="1mo"))
+worker0 = Worker()
+worker0.add_product(fish, 3)
+worker0.add_product(apple, 2)
 
-    party1.party_detail()
+worker1 = Worker()
+worker1.add_product(fish, 2)
+worker1.add_product(apple, 4)
+
+ppf = PPF(resource=36)
+ppf.add_worker(worker0)
+ppf.add_worker(worker1)
+ppf.add_worker(worker1)
+ppf.add_worker(worker1)
+
+p_vec = ppf._production_vector()
+print(p_vec)
---
+++ examples/test_matplotlib.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3]
+y = [2, 4, 6]
+z = [1, 2, 3]
+
+plt.plot(x, y, z)
+plt.show()
+
