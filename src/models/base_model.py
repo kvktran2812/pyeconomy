5a0744dba792488373c8ec717f5b147c6ec23143
kvktran2812 <kvktran@gmail.com>
2023-08-04 15:42:20 -0400

    create linear_model class

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,11 +1,47 @@
+import abc
 from abc import ABC
+import networkx as nx
+import logging
 
 
-# TODO: add docstring
-class Base_Model(ABC):
+class LinearModel:
     def __init__(self):
+        self.data = {}
+        self.steps = []
+
+    def run(self):
+        for s in self.steps:
+            args = self.get_data(s["inputs"])
+            f = s["function"]
+            output = f(*args)
+
+            if len(s["outputs"]) == 1:
+                self.data[s["outputs"][0]] = output
+            else:
+                for i, v in enumerate(s["outputs"]):
+                    self.data[v] = output[i]
+
+    def log(self):
         return
 
-    def apply(self):
+    def _save_data(self):
+
         return
+
+    def _get_data(self, inputs):
+        args = []
+        for i in inputs:
+            if i in self.data:
+                args.append(self.data[i])
+            else:
+                args.append(i)
+        return args
+
+    def add_step(self, function, inputs, outputs):
+        step = {
+            "function": function,
+            "inputs": inputs,
+            "outputs": outputs,
+        }
+        self.steps.append(step)
 
