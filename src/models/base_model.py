2be015c1dc1d90ffc560684e11de9d30b647b022
kvktran2812 <kvktran@gmail.com>
2023-08-05 12:15:55 -0400

    add model step verification

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -4,7 +4,12 @@ import networkx as nx
 import logging
 
 
+# TODO: Add docstring
 class LinearModel:
+    """
+    A linear model class is used to represent computing of economic model. The computation is base on the steps of the
+    model, linearly from inputs step by step to outputs.
+    """
     def __init__(self):
         self.data = {}
         self.steps = []
@@ -36,11 +41,26 @@ class LinearModel:
                 args.append(i)
         return args
 
+    @staticmethod
+    def _step_verification(function, inputs, outputs):
+        if not callable(function):
+            return False, "function parameter is not callable"
+        if not hasattr(inputs, "__iter__"):
+            return False, "inputs parameter is not iterable"
+        if not hasattr(outputs, "__iter__") and type(outputs) is not str:
+            return False, "outputs parameter is not iterable or string"
+        return True, "No error"
+
     def add_step(self, function, inputs, outputs):
-        step = {
-            "function": function,
-            "inputs": inputs,
-            "outputs": outputs,
-        }
-        self.steps.append(step)
+        status, msg = self._step_verification(function, inputs, outputs)
+        if status:
+            step = {
+                "function": function,
+                "inputs": inputs,
+                "outputs": outputs,
+            }
+            self.steps.append(step)
+        else:
+            raise ValueError(f"Error: {msg}, can not add this step")
+
 
