14b05addbf672fc9b744f2c21c1b9e9c276d7f4b
kvktran2812 <kvktran@gmail.com>
2023-08-07 22:51:09 -0400

    add test logging

---
+++ tests/test_utils/test_log.py
@@ -0,0 +1,23 @@
+import pytest
+from src.utils.log import *
+
+
+def test_custom_formatter():
+    assert CustomFormatter.grey == "\x1b[37m"
+    assert CustomFormatter.yellow == "\x1b[33;20m"
+    assert CustomFormatter.red == "\x1b[31;20m"
+    assert CustomFormatter.bold_red == "\x1b[31;1m"
+    assert CustomFormatter.reset == "\x1b[0m"
+
+
+@pytest.mark.parametrize(
+    "name, file_handler, no_handler",
+    [
+        ("MyModel", False, 1),
+        ("MyModel", True, 2),
+    ],
+)
+def test_model_logger(name, file_handler, no_handler):
+    logger = ModelLogger(name=name, log_to_file=file_handler)
+    assert logger.name == name
+    assert len(logger.handlers) == no_handler
