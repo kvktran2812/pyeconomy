897d387f70fb10e4a924c960b5aa622568928113
kvktran2812 <kvktran@gmail.com>
2023-08-08 23:09:43 -0400

    initial economic utility

---
+++ src/economy/utility.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Utility:
+    def __init__(self, utils, price):
+        self.utils = utils
+        self.price = price
+        return
+
+    def marginal_util_per_unit(self):
+        return
+
+    def calc_marginal_utils(self):
+        return
+
+    def convert_to_demand(self):
+        return
