398186937fcb281d5caafde96111799d5d030e67
kvktran2812 <kvktran@gmail.com>
2024-06-21 07:28:44 -0400

    add spectral radius

--- src/utils/eigen.py
+++ src/utils/eigen.py
@@ -38,6 +38,50 @@ def spectral(A):
     return U, D, U.T
 
 
+def worker_dynamic(alpha, beta):
+    eigen_values = np.array([1, 1 - alpha - beta])
+    eigen_vectors = np.array([
+        [1, 1], 
+        [-alpha, beta]
+    ])
+    return eigen_values, eigen_vectors
+
+
+def worker_dynamic_decomposition(alpha, beta):
+    D = np.array([
+        [1, 0],
+        [0, 1 - alpha - beta]
+    ])
+    E = np.array([
+        [1, -alpha],
+        [1, beta],
+    ])
+    return E, D, np.linalg.inv(E)
+
+
+def worker_dynamic_power_matrix(alpha, beta, m):
+    factor = 1 / (alpha + beta)
+    lambda_2 = 1 - alpha - beta
+    power = np.array([
+        [beta + alpha * lambda_2 ^ m, alpha * lambda_2 ^ m],
+        [beta * (1 - lambda_2)^m, alpha + beta * (lambda_2) ^ m]
+    ])
+
+    return factor * power
+
+
+def spectral_radius(A):
+    """
+    Function to calculate spectral radius of a matrix
+
+    Args:
+        A (np.array): Matrix A to calculate spectral radius from
+
+    Returns:
+        int: Spectral radius of matrix A (scalar value)
+    """
+    return np.max(np.abs(np.linalg.eigvals(A)))
+
 if __name__ == "__main__":
     A = np.array([[0, -1], [1, 0]])
     
